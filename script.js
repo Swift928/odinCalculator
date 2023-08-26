aeb8019166d40c668bdec35ead9a534b60eb582c
Keith Taylor <68087065+Swift928@users.noreply.github.com>
2023-08-26 16:47:02 -0500

    clear and number inputs are working

--- script.js
+++ script.js
@@ -12,9 +12,7 @@ function operate(operator, num1, num2, ...rest) {
     }
 }
 
-console.log(operate('/' , 100, 5))
 
-// console.log(operate())
 
 function addition(...rest) {
     let computed = rest.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
@@ -38,3 +36,59 @@ function divide(...rest) {
     let computed = rest.reduce((accumulator, currentValue) => accumulator / currentValue)
     return computed
 }
+
+let firstValue;
+let secondValue;
+
+const calculatorUi = document.querySelector('.calculator-ui')
+
+const display =  document.querySelector('.calc-display')
+const numButtons = calculatorUi.querySelectorAll('.number')
+const decimalButton = calculatorUi.querySelector('.decimal')
+
+const numberRegex = /^[0-9]$/;
+const decimalRegex = /^\.$/;
+
+const clearButton = document.querySelector('.ac-button')
+
+let displayValue = parseInt(display.innerHTML)
+
+
+
+clearButton.addEventListener('click', () => {
+    display.innerHTML = displayValue
+
+    if (clearButton.innerHTML == 'C') {
+        clearButton.innerHTML = 'AC'
+    }
+    firstA = []
+})
+
+
+let firstA = [];
+
+let result = firstA ? 'its true' : 'its not true';
+console.log(result)
+
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+    const numValue = parseInt(button.innerHTML)
+    console.log(typeof numValue)
+    
+        if (numberRegex.test(button.innerHTML) || decimalRegex.test(button.innerHTML)) {
+        clearButton.innerHTML = 'C'
+        firstA.push(numValue)
+        display.innerHTML = parseInt(firstA.join(''))
+        // console.log(typeof numValue)
+        }        
+})
+})
+
+decimalButton.addEventListener('click', () => {
+    if (!firstA.length) {
+        clearButton.innerHTML = 'C'
+        firstA.push(decimalButton)
+    } else { firstA.push('.')}
+})
+
